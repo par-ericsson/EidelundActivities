cf8e995eb97add7b4dbaa4aa9df66b0bf5f1a90b
Pär Ericsson <ericsson@home.se>
2021-09-25 20:07:42 +0200

    Added attendees, automapper, many to many

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Application.Activities;
 using AutoMapper;
 using Domain;
 
@@ -8,6 +10,12 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUsername, opt => opt.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Username, opt => opt.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, opt => opt.MapFrom(s => s.AppUser.Bio));
         }
     }
 }
