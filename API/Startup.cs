852c7d2876ac71c181c9913085039201b4675f0d
Pär Ericsson <ericsson@home.se>
2021-10-19 10:38:45 +0200

    Added create handler, a signalr hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ using FluentValidation.AspNetCore;
 using API.Middleware;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using API.SignalR;
 
 namespace API
 {
@@ -70,6 +71,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -175,6 +175,9 @@ export default class ActivityStore {
         } finally {
             runInAction(() => this.loading = false);
         }
+    }
 
+    clearSelectedActivity = () => {
+        this.selectedActivity = undefined;
     }
 }
